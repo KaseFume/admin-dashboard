ff6c6aecd96fb549614153ba102b8f04b6926909
Min Thant Ko <78795759+KaseFume@users.noreply.github.com>
2024-10-27 16:15:02 +0630

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class EmailForm(forms.Form):
-    email = forms.EmailField()
-
-class OTPForm(forms.Form):
-    otp_code = forms.CharField(max_length=6)
--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('send-otp/', send_otp, name='send_otp'),
-    path('verify-otp/', verify_otp, name='verify_otp'),
-    path('resend-otp/', resend_otp, name='resend_otp'),  # Ensure this line is present
-]
-
