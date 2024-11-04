0d60b1cfe9ec180ad8d0df714104c23096ea753a
Min Thant Ko <78795759+KaseFume@users.noreply.github.com>
2024-11-04 16:57:34 +0630

    Fixed

--- data/templatetags/custom_filters.py
+++ data/templatetags/custom_filters.py
@@ -14,8 +14,16 @@ register.filter('extract_number',extract_number)
 def convert_to_gmt630(value):
     if not value:
         return ''
-    # Assuming 'value' is a datetime object
-    local_tz = pytz.timezone('Asia/Yangon')  # Replace with your desired timezone
-    gmt630_time = value.astimezone(local_tz)
-    return gmt630_time.strftime('%b %d, %Y, %I:%M %p')  # Format as needed
+    # Assuming 'value' is a naive datetime object (without timezone info)
+    # or aware datetime object (with timezone info)
+    if timezone.is_aware(value):
+        # If the datetime is already timezone-aware, convert it to the desired timezone
+        local_tz = pytz.timezone('Asia/Yangon')
+        gmt630_time = value.astimezone(local_tz)
+    else:
+        # If it's naive, assume it's in UTC and localize it to the desired timezone
+        local_tz = pytz.timezone('Asia/Yangon')
+        gmt630_time = local_tz.localize(value)
+
+    return gmt630_time.strftime('%b %d, %Y, %I:%M %p')   # Format as needed
 register.filter('convert_to_gmt630',convert_to_gmt630)
