6f63f9771bfa6b2e62d2501c14dfb201c10d1bd8
Min Thant Ko <78795759+KaseFume@users.noreply.github.com>
2024-11-06 21:09:32 +0630

    PLEASEEEEE

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,21 @@
-python3 -m venv venv          # Create a virtual environment using python3
-source venv/bin/activate       # Activate the virtual environment
-pip install -r requirements.txt # Install dependencies
-python3 manage.py collectstatic --noinput # Collect static files
+python3 -m venv venv                  # Create a virtual environment
+source venv/bin/activate               # Activate the virtual environment
+
+# Install each package explicitly
+pip install asgiref==3.8.1
+pip install Django==5.1.2
+pip install django-otp==1.5.4
+pip install numpy==2.1.0
+pip install pandas==2.2.3
+pip install python-dateutil==2.9.0.post0
+pip install pytz==2024.2
+pip install sqlparse==0.5.1
+pip install twilio==9.3.3
+pip install tzdata==2024.1
+pip install whitenoise==6.8.2
+pip install XlsxWriter==3.2.0
+pip install zipp==3.20.0
+pip install six==1.16.0
+
+# Collect static files for Django
+python3 manage.py collectstatic --noinput
