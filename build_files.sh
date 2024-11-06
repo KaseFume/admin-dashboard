e353bfac9e7961dfa6ae5663df1e17f005e21ca2
Min Thant Ko <78795759+KaseFume@users.noreply.github.com>
2024-11-06 21:05:34 +0630

    PLEASEEE WORK

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-py manage.py collectstatic
+python3 -m venv venv          # Create a virtual environment using python3
+source venv/bin/activate       # Activate the virtual environment
+pip install -r requirements.txt # Install dependencies
+python3 manage.py collectstatic --noinput # Collect static files
